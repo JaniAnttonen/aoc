9a8bf225163f2c85b04ab2fd3ee26a750067b911
Jani Anttonen <jantto@utu.fi>
2019-12-03 00:51:41 +0200

    add opcode error, solve day 2 part 1

--- 2019/day2/src/main.rs
+++ 2019/day2/src/main.rs
@@ -2,7 +2,22 @@ extern crate csv;
 use csv::ReaderBuilder;
 use csv::StringRecord;
 use std::error::Error;
-//use std::str::FromStr;
+use std::fmt;
+
+#[derive(Debug)]
+struct OpCodeError;
+
+impl fmt::Display for OpCodeError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Invalid opcode encountered!")
+    }
+}
+
+impl Error for OpCodeError {
+    fn description(&self) -> &str {
+        "Invalid opcode encountered!"
+    }
+}
 
 fn read_input(file_path: String) -> Result<Vec<StringRecord>, Box<dyn Error>> {
     let mut reader = ReaderBuilder::new()
@@ -16,19 +31,45 @@ fn read_input(file_path: String) -> Result<Vec<StringRecord>, Box<dyn Error>> {
     Ok(output)
 }
 
-fn run_row(row: StringRecord) -> Result<Vec<i32>, Box<dyn Error>> {
-    let program_state: Vec<i32> = row.deserialize(None).unwrap();
-    Ok(program_state)
+fn run_row(row: Vec<usize>) -> Result<Vec<usize>, OpCodeError> {
+    const PROG_SIZE: usize = 4;
+    let mut prog: Vec<usize> = row.clone();
+
+    // Restore gravity assist to 1202 program alarm
+    prog[1] = 12;
+    prog[2] = 2;
+
+    let mut instruction: usize = 0;
+    let mut index: usize = instruction * PROG_SIZE;
+
+    while index + 3 < row.len() {
+        let opcode = prog[index];
+        let input1 = prog[index + 1];
+        let input2 = prog[index + 2];
+        let output = prog[index + 3];
+        match opcode {
+            1 => prog[output] = prog[input1] + prog[input2],
+            2 => prog[output] = prog[input1] * prog[input2],
+            99 => break,
+            _ => return Err(OpCodeError),
+        }
+        instruction += 1;
+        index = instruction * PROG_SIZE;
+    }
+
+    Ok(prog)
 }
 
 fn main() {
     let file_path: String = String::from("./src/input");
     let code_vec: Vec<StringRecord> = read_input(file_path).unwrap();
-    let mut row: StringRecord;
-    let mut result: Vec<i32>;
+
+    let mut row: Vec<usize>;
+    let mut result: Vec<usize>;
 
     for rec in code_vec {
-        row = rec;
+        row = rec.deserialize(None).unwrap();
         result = run_row(row).unwrap();
+        println!("Program result: {:?}", result[0]);
     }
 }
