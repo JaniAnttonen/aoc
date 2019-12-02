64a1e315eb839db640679b7a53366c2f1b78e49d
Jani Anttonen <jantto@utu.fi>
2019-12-02 16:22:58 +0200

    calculates something but not the right answer yet

--- 2019/day1/src/main.rs
+++ 2019/day1/src/main.rs
@@ -1,28 +1,48 @@
 extern crate math;
 use math::round;
 use std::fs::File;
-use std::io::prelude::*;
+use std::io::BufRead;
 use std::io::{self, BufReader};
+
+fn create_file_reader(file_path: String) -> io::Result<BufReader<File>> {
+    let file = File::open(file_path)?;
+    Ok(BufReader::new(file))
+}
 
 fn calc_fuel_for_mass(module_mass: f64) -> f64 {
     round::floor(module_mass / 3.0, 0) - 2.0
 }
 
-fn part_one() -> io::Result<()> {
-    let f = File::open("./src/input")?;
-    let f = BufReader::new(f);
+fn part_one(file_path: String) -> io::Result<(f64)> {
+    let reader = create_file_reader(file_path).unwrap();
     let mut fuel_needed: f64 = 0.0;
 
-    for line in f.lines() {
-        let line_string: String = line.unwrap();
-        let module_mass: f64 = line_string.parse().unwrap();
-        fuel_needed += calc_fuel_for_mass(module_mass)
+    for line in reader.lines() {
+        let module_mass: f64 = line.unwrap().parse().unwrap();
+        fuel_needed += calc_fuel_for_mass(module_mass);
     }
 
-    Ok((fuel_needed))
+    Ok(fuel_needed)
+}
+
+fn part_two(module_fuel: f64) -> io::Result<(f64)> {
+    let mut extra_fuel_required = calc_fuel_for_mass(module_fuel);
+    let mut fuel_required_total = module_fuel;
+    loop {
+        fuel_required_total += extra_fuel_required;
+        extra_fuel_required = calc_fuel_for_mass(extra_fuel_required);
+        if extra_fuel_required <= 0.0 {
+            break;
+        }
+    }
+    Ok(fuel_required_total)
 }
 
 fn main() {
-    let first_result = part_one();
-    println!("Part one: {}", first_result)
+    let file_path: String = String::from("./src/input");
+    let first_result = part_one(file_path).unwrap();
+    println!("Part one: {}", first_result);
+
+    let second_result = part_two(first_result).unwrap();
+    println!("Part two: {}", second_result);
 }
