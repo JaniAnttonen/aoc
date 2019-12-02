d4c56c5f37f0f1370800a007243d0dd114e9ad14
Jani Anttonen <jantto@utu.fi>
2019-12-03 01:13:53 +0200

    solve part two

--- 2019/day2/src/main.rs
+++ 2019/day2/src/main.rs
@@ -31,13 +31,13 @@ fn read_input(file_path: String) -> Result<Vec<StringRecord>, Box<dyn Error>> {
     Ok(output)
 }
 
-fn run_row(row: Vec<usize>) -> Result<Vec<usize>, OpCodeError> {
+fn run_row(row: Vec<usize>, noun: usize, verb: usize) -> Result<Vec<usize>, OpCodeError> {
     const PROG_SIZE: usize = 4;
     let mut prog: Vec<usize> = row.clone();
 
     // Restore gravity assist to 1202 program alarm
-    prog[1] = 12;
-    prog[2] = 2;
+    prog[1] = noun;
+    prog[2] = verb;
 
     let mut instruction: usize = 0;
     let mut index: usize = instruction * PROG_SIZE;
@@ -64,12 +64,26 @@ fn main() {
     let file_path: String = String::from("./src/input");
     let code_vec: Vec<StringRecord> = read_input(file_path).unwrap();
 
-    let mut row: Vec<usize>;
-    let mut result: Vec<usize>;
+    let row: Vec<usize> = code_vec[0].deserialize(None).unwrap();
 
-    for rec in code_vec {
-        row = rec.deserialize(None).unwrap();
-        result = run_row(row).unwrap();
-        println!("Program result: {:?}", result[0]);
+    let result1: Vec<usize> = run_row(row.clone(), 12, 2).unwrap();
+    println!("Part One: {:?}", result1[0]);
+
+    let mut corr_noun: usize = 0;
+    let mut corr_verb: usize = 0;
+    const WANTED_RESULT: usize = 19690720;
+    let mut result2: Vec<usize>;
+
+    for noun in 0..99 {
+        for verb in 0..99 {
+            result2 = run_row(row.clone(), noun, verb).unwrap();
+            if result2[0] == WANTED_RESULT {
+                corr_noun = noun;
+                corr_verb = verb;
+                break;
+            }
+        }
     }
+
+    println!("Part Two: {}", 100 * corr_noun + corr_verb);
 }
