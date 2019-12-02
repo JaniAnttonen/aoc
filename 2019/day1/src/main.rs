e78b37bb7acd4ac5e6b9e14f2a28f87c0756e74b
Jani Anttonen <jantto@utu.fi>
2019-12-02 20:48:24 +0200

    solve part two

--- 2019/day1/src/main.rs
+++ 2019/day1/src/main.rs
@@ -15,34 +15,46 @@ fn calc_fuel_for_mass(module_mass: f64) -> f64 {
 
 fn part_one(file_path: String) -> io::Result<(f64)> {
     let reader = create_file_reader(file_path).unwrap();
+    let mut module_mass: f64;
     let mut fuel_needed: f64 = 0.0;
 
     for line in reader.lines() {
-        let module_mass: f64 = line.unwrap().parse().unwrap();
+        module_mass = line.unwrap().parse().unwrap();
         fuel_needed += calc_fuel_for_mass(module_mass);
     }
 
     Ok(fuel_needed)
 }
 
-fn part_two(module_fuel: f64) -> io::Result<(f64)> {
-    let mut extra_fuel_required = calc_fuel_for_mass(module_fuel);
-    let mut fuel_required_total = module_fuel;
-    loop {
-        fuel_required_total += extra_fuel_required;
-        extra_fuel_required = calc_fuel_for_mass(extra_fuel_required);
-        if extra_fuel_required <= 0.0 {
-            break;
+fn part_two(file_path: String) -> io::Result<(f64)> {
+    let reader = create_file_reader(file_path).unwrap();
+    let mut module_mass: f64;
+    let mut fuel_required: f64;
+    let mut extra_fuel_required: f64;
+    let mut fuel_required_total: f64 = 0.0;
+
+    for line in reader.lines() {
+        module_mass = line.unwrap().parse().unwrap();
+        fuel_required = calc_fuel_for_mass(module_mass);
+        extra_fuel_required = calc_fuel_for_mass(fuel_required);
+        loop {
+            if extra_fuel_required <= 0.0 {
+                break;
+            }
+            fuel_required += extra_fuel_required;
+            extra_fuel_required = calc_fuel_for_mass(extra_fuel_required);
         }
+        fuel_required_total += fuel_required;
     }
+
     Ok(fuel_required_total)
 }
 
 fn main() {
     let file_path: String = String::from("./src/input");
-    let first_result = part_one(file_path).unwrap();
+    let first_result = part_one(file_path.clone()).unwrap();
     println!("Part one: {}", first_result);
 
-    let second_result = part_two(first_result).unwrap();
+    let second_result = part_two(file_path.clone()).unwrap();
     println!("Part two: {}", second_result);
 }
