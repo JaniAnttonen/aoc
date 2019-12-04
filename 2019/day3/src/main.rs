c18472e609fb2afdb928a5d58f1a79037fb56e7a
Jani Anttonen <jantto@utu.fi>
2019-12-04 12:31:32 +0200

    miiiiiight work

--- 2019/day3/src/main.rs
+++ 2019/day3/src/main.rs
@@ -9,10 +9,39 @@ struct Point {
     y: isize,
 }
 
+impl PartialEq for Point {
+    fn eq(&self, other: &Self) -> bool {
+        self.x == other.x && self.y == other.y
+    }
+}
+
 #[derive(Debug, Clone, Copy)]
 struct Line {
     from: Point,
     to: Point,
+}
+
+impl Line {
+    fn is_vertical(&self) -> bool {
+        self.from.x == self.to.x
+    }
+    fn intersects(&self, other: &Self) -> bool {
+        let mut collision: bool = false;
+        if self.from == other.from || self.to == other.to {
+            collision = false;
+        }
+        if self.is_vertical() == other.is_vertical() {
+            collision = false;
+        }
+        if (self.from.x < other.to.x && self.from.x > other.from.x
+            || self.from.x > other.to.x && self.from.x < other.from.x)
+            && (self.from.y < other.from.y && self.to.y > other.from.y
+                || self.from.y > other.from.y && self.to.y < other.from.y)
+        {
+            collision = true;
+        }
+        collision
+    }
 }
 
 fn read_input(file_path: String) -> Result<Vec<StringRecord>, Box<dyn Error>> {
@@ -53,14 +82,36 @@ fn calc_lines(path: Vec<String>) -> Result<Vec<Line>, Box<dyn Error>> {
     Ok(lines)
 }
 
-fn find_intersections(lines: Vec<Line>) -> Result<Vec<Point>, Box<dyn Error>> {}
+fn find_intersections(mut lines: Vec<Line>) -> Result<Vec<Point>, Box<dyn Error>> {
+    let mut intersections: Vec<Point> = Vec::new();
+    loop {
+        match lines.pop() {
+            None => {
+                println!("No more lines to check!");
+                break;
+            }
+            Some(line) => {
+                for checked_line in &lines {
+                    let intersects = checked_line.intersects(&line);
+                    if intersects {
+                        println!("Intersection found for {:?} and {:?}!", line, checked_line);
+                    //intersections.push(calc_intersection(checked_line, line));
+                    } else {
+                        println!("No intersections found!");
+                    }
+                }
+            }
+        }
+    }
+    Ok(intersections)
+}
 
 fn main() {
     let file_path: String = String::from("./src/input");
     let prog_input: Vec<StringRecord> = read_input(file_path).unwrap();
-    let mut lines: Vec<Line> = Vec::new();
     for row in prog_input {
         let path: Vec<String> = row.deserialize(None).unwrap();
         let lines_for_input: Vec<Line> = calc_lines(path).unwrap();
+        let intersections_for_input: Vec<Point> = find_intersections(lines_for_input).unwrap();
     }
 }
